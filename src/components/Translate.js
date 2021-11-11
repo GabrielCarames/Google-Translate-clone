548e4092671c4fd89b9a11d3995f8035539c81d4
Gabriel Carames <49535753+GabrielCarames@users.noreply.github.com>
2021-11-11 13:27:53 -0300

    Get all lenguages request and lenguages provisory object

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,7 +1,11 @@
-const Translate = () =>{
+import useTranslateHelper from "../hooks/useTranslateHelper"
+
+const Translate = () => {
+    const { getLanguages } = useTranslateHelper()
+
     return (
         <div className="body-translate-container">
-            
+            <button onClick={() => getLanguages()}>dame idiomas</button>
         </div>
     )
 }
