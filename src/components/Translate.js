a2863c12a85ce693aeb29787cd61e0be3a995e2d
Erick Quispe <erickjoel10x@gmail.com>
2021-11-11 22:02:20 -0300

    translate container + styles

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -2,11 +2,32 @@ import useTranslateHelper from "../hooks/useTranslateHelper"
 
 const Translate = () => {
     const { getLanguages, getTraduction, setTextToTranslate, setTarget, setSource } = useTranslateHelper()
-
+    const lista = () => {
+        return (<ul className="translate-list">
+        <li className="list__item"><p className="list__text">Ingles</p></li>
+        <li className="list__item"><p className="list__text">Español</p></li>
+    </ul>)
+    }
     return (
-        <div className="body-translate-container">
+        <article className="body-translate-container translate">
+            <header className="translate-header">
+                <div className="translate__left-navbar">
+                    {lista()}
+                </div>
+                <div className="translate__right-navbar">
+                    {lista()}
+                </div>
+            </header>
+            <body className="translate-body">
+                <div className="translate__text-to-translate">
+                    <textarea className="translate__input" type="text"></textarea>
+                </div>
+                <div className="translate__traduction">
+                    <p className="translate__text-result">Traducción</p>
+                </div>
+            </body>
             <button onClick={() => getLanguages()}>dame idiomas</button>
-        </div>
+        </article>
     )
 }
 export default Translate
