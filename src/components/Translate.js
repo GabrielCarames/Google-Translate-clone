5d672a6b63e124050d1697c82591cb868c43a418
Erick Quispe <erickjoel10x@gmail.com>
2021-11-24 09:52:56 -0300

    change icon mamushka

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -24,15 +24,14 @@ const Translate = () => {
                         <li className={languagesCosa.target && !changeLanguage ? "list__item--target" : changeLanguage === "source" ? "list__item--target" : "list__item"} onClick={() => changeLanguage === "target" && changeLanguage && setChangeLanguage("source")} ><p className="list__text">{languages && wholanguage("target")}</p></li>
                         <li className={changeLanguage === "target" ? "list__item--target" : "list__item"} onClick={() => (changeLanguage !== "target" && setChangeLanguage("target")) }><p className="list__text" >{languages && wholanguage("source") }</p></li>
                         <li className="list__arrow" onClick={() => {showList ? setShowList(false) : setShowList(true); setResults(false)}}><i class="fas fa-chevron-down"></i></li>
-                        <button onClick={() => {dispatch({type: '@setSource', payload: languagesCosa.target}); dispatch({type: '@setTarget', payload: languagesCosa.source})}}> elotudo</button>
                     </ul>
                 </div>
                 <div className="translate__right-navbar">
                     <ul className="translate-list">
+                        <li className="list__change-language" onClick={() => {dispatch({type: '@setTarget', payload: languagesCosa.source}); dispatch({type: '@setSource', payload: languagesCosa.target})}}><i className="fas fa-exchange-alt"></i></li>
                         <li className={languagesCosa.target && !changeLanguage ? "list__item--target" : changeLanguage === "source" ? "list__item--target" :"list__item"} onClick={() => changeLanguage === "target" && changeLanguage && setChangeLanguage("source")} ><p className="list__text">{languages && wholanguage("source")}</p></li>
                         <li className={changeLanguage === "target" ? "list__item--target" : "list__item"} onClick={() => (changeLanguage !== "target" && setChangeLanguage("target"))}><p className="list__text" >{languages && wholanguage("target") }</p></li>
                         <li className="list__arrow" onClick={() => {showList ? setShowList(false) : setShowList(true); setResults(false)}}><i class="fas fa-chevron-down"></i></li>
-                        <button onClick={() => {dispatch({type: '@setTarget', payload: languagesCosa.source}); dispatch({type: '@setSource', payload: languagesCosa.target})}}> elotudo</button>
                     </ul>
                 </div>
             </header>
