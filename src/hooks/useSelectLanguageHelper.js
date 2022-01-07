9069c53a6f66a313518b337e90d12949ce21c796
Erick Quispe <erickjoel10x@gmail.com>
2022-01-07 12:45:11 -0300

    select language same language in source or target fixed

--- src/hooks/useSelectLanguageHelper.js
+++ src/hooks/useSelectLanguageHelper.js
@@ -1,6 +1,7 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function useSelectLanguageHelper(languageInUse, setLanguageInUse) {
+    const languagesState = useSelector(state => state.changeLanguageReducer)
     const dispatch = useDispatch()
 
     const selectLanguage = (showList, item) => {
@@ -22,14 +23,17 @@ export default function useSelectLanguageHelper(languageInUse, setLanguageInUse)
             let actualSource = languageInUse.source
             switch (showList) {
                 case "target":
+                    if(languagesState.extraSource.name === languageInUse.source.name && languagesState.extraTarget.name === languageInUse.target.name) dispatch({type: "@changeLanguage", payload: "target"})
                     dispatch({type:"@setExtraTargetLanguage", payload: item})
+                    
                     if(languageInUse.source.name === item.name) setLanguageInUse({...languageInUse, ...{source: actualTarget}, ...{target: item}})
-                    else setLanguageInUse({...languageInUse, ...{source: actualSource}, ...{target: item}})
+                    else setLanguageInUse({...languageInUse, ...{target: item}})
                     break;
                 case "source": 
+                if(languagesState.extraSource.name === languageInUse.source.name && languagesState.extraTarget.name === languageInUse.target.name) dispatch({type: "@changeLanguage", payload: "source"})
                     dispatch({type:"@setExtraSourceLanguage", payload: item})
-                    if(languageInUse.target.name === item.name) setLanguageInUse({...languageInUse, ...{source: item}, ...{target: actualSource}})   
-                    else setLanguageInUse({...languageInUse, ...{source: item}, ...{target: actualTarget}})
+                    if(languageInUse.target.name === item.name) setLanguageInUse({...languageInUse, ...{source: item}, ...{target: actualSource}})
+                    else setLanguageInUse({...languageInUse, ...{source: item}})
                     break;
                 default:
                     break;
